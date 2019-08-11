795cb6a862893f557b4be61b1a5e58de56848d40
algogz <wangyaojun@gmail.com>
2019-08-11 21:18:42 +0800

    parse and get qulified class name

--- src/main/java/org/javaparser/examples/chapter5/Foo.java
+++ src/main/java/org/javaparser/examples/chapter5/Foo.java
@@ -3,5 +3,6 @@ package org.javaparser.examples.chapter5;
 import org.javaparser.examples.chapter4.*;
 
 class Foo {
+    public String name = "abc";
     Bar bar;
 }
--- src/main/java/org/javaparser/examples/chapter5/ResolveTypeInContext.java
+++ src/main/java/org/javaparser/examples/chapter5/ResolveTypeInContext.java
@@ -3,6 +3,8 @@ package org.javaparser.examples.chapter5;
 import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.FieldDeclaration;
+import com.github.javaparser.ast.expr.NameExpr;
+import com.github.javaparser.ast.type.ClassOrInterfaceType;
 import com.github.javaparser.symbolsolver.JavaSymbolSolver;
 import com.github.javaparser.symbolsolver.javaparser.Navigator;
 import com.github.javaparser.symbolsolver.model.resolution.TypeSolver;
@@ -14,8 +16,10 @@ import java.io.File;
 
 public class ResolveTypeInContext {
 
-    private static final String FILE_PATH = "src/main/java/org/javaparser/examples/chapter5/Foo.java";
-    private static final String SRC_PATH = "src/main/java";
+    private static final String FILE_PATH = "/Users/algo/dev/java/javaparser-maven-sample/src/main/java/algo/test/impl/Dog.java";
+    private static final String SRC_PATH = "/Users/algo/dev/java/javaparser-maven-sample/src/main/java";
+//    private static final String FILE_PATH = "src/main/java/org/javaparser/examples/chapter5/Foo.java";
+//    private static final String SRC_PATH = "src/main/java";
 
     public static void main(String[] args) throws Exception {
         TypeSolver reflectionTypeSolver = new ReflectionTypeSolver();
@@ -33,6 +37,17 @@ public class ResolveTypeInContext {
 
         CompilationUnit cu = StaticJavaParser.parse(new File(FILE_PATH));
 
+        System.out.println("\n==== NameExpr");
+        cu.findAll(NameExpr.class).forEach(c -> {
+            System.out.println(c.calculateResolvedType().asReferenceType().getQualifiedName());
+        });
+
+        System.out.println("\n==== ClassOrInterfaceType");
+        cu.findAll(ClassOrInterfaceType.class).forEach(c -> {
+            System.out.println(c.resolve().asReferenceType().getQualifiedName());
+        });
+
+        System.out.println("\n==== FieldDeclaration");
         FieldDeclaration fieldDeclaration = Navigator.findNodeOfGivenClass(cu, FieldDeclaration.class);
 
         System.out.println("Field type: " + fieldDeclaration.getVariables().get(0).getType()
